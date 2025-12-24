259d0f8807916e8714f590280488d689f78bceb6
igorlde <igorligeiro46@gmail.com>
2025-12-24 10:52:26 -0300

    feat: I add new fature that search a contentfor number page and search for a string

--- src/main/java/entity/PdfInfoClass.java
+++ src/main/java/entity/PdfInfoClass.java
@@ -7,6 +7,8 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PdfInfoClass {
 
@@ -26,11 +28,36 @@ public class PdfInfoClass {
         }
         return textPdf;
     }
-    //create a function for search for a String type a search chapter for name
 
+    //create a function for search for a String type a search chapter for name
+    public static List<Integer> searchContentPage(String search, String pathOfPDf){
+        Path pathPdf = Paths.get(pathOfPDf);
+       // String textPageFinds;
+        List<Integer> pageFinds = new ArrayList<>();
+        try(PDDocument document = Loader.loadPDF(pathPdf.toFile())){
+            PDFTextStripper stripper = new PDFTextStripper();
+            for(int i = 0;i <= document.getNumberOfPages();i++){
+                    stripper.setStartPage(i);
+                    stripper.setEndPage(i);
+                    String textPage = stripper.getText(document);
+                    if(textPage.toLowerCase().contains(search.toLowerCase())){
+                        pageFinds.add(i);//count total result
+                        //textPageFinds = stripper.getText(document);
+                        //System.out.println(textPageFinds);//here get every content taht have in the page
+                    }
+            }
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+        return pageFinds;
+    }
     public static void main(String[] args) {
-        String document = searchPagePdf(1, "/home/igor/Desktop/testPDF.pdf");
-        System.out.println(document);
+//        String document = searchPagePdf(24, "/home/igor/Livros/The.C.Programming.Language.2Nd.Ed Prentice.Hall.Brian.W.Kernighan.and.Dennis.M.Ritchie..pdf");
+        List<Integer> pages = searchContentPage("int ", "/home/igor/Livros/The.C.Programming.Language.2Nd.Ed Prentice.Hall.Brian.W.Kernighan.and.Dennis.M.Ritchie..pdf");
+//        System.out.println(document);
+        for (int i = 0;i < pages.size();i++){
+          System.out.println(pages);
+        }
     }
 
 }
